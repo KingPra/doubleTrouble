6d02f962ec3c3fc81947e070fb6a2ee6d47c17e0
King Pra <kingi.pra@gmail.com>
2019-05-24 14:35:38 -0500

    commit test

--- src/dice/RollDiceTest.java
+++ src/dice/RollDiceTest.java
@@ -1,15 +1,15 @@
 package dice;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class RollDiceTest {
-    @Disabled("Comment")
+	@Disabled("Comment")
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		fail("Hey lilShell, this is a commit test- King");
 	}
 
 }
