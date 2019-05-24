1feaba34c31361e48a0195e195c64c49dbb63c42
lilshell73 <lilshell73@yahoo.com>
2019-05-24 14:32:12 -0500

    disabled test

--- src/dice/RollDiceTest.java
+++ src/dice/RollDiceTest.java
@@ -2,10 +2,11 @@ package dice;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class RollDiceTest {
-
+    @Disabled("Comment")
 	@Test
 	void test() {
 		fail("Not yet implemented");
