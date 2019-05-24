93b5919524c077ad7d83165a832fe1218bfd38e3
King Pra <kingi.pra@gmail.com>
2019-05-24 14:03:32 -0500

    junit test shell added to project

---
+++ src/dice/RollDiceTest.java
@@ -0,0 +1,14 @@
+package dice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RollDiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
