3b785646794125fa4ebdbc47cf5261338091ea77
King Pra <kingi.pra@gmail.com>
2019-05-30 09:49:44 -0500

    minor cleanup

--- src/dice/RollDice.java
+++ src/dice/RollDice.java
@@ -4,7 +4,7 @@ public class RollDice {
 	public static void main(String[] args) {
 		// calls play game method and passes in dice() as an int argument and
 		// firstRoll() as a boolean argument
-//		playGame(dice(), firstRoll());
+		playGame(dice(), firstRoll());
 	}
 
 	// these variables are global to track throughout game play;
@@ -23,7 +23,7 @@ public class RollDice {
 		}
 	}
 
-	// generates a random number between 1-6;
+	// generates a random number between 2-12;
 	public static int dice() {
 		int num = (int) (3 + Math.random() * 10);
 		return num;
@@ -32,16 +32,15 @@ public class RollDice {
 	// game logic
 	public static String playGame(int dice, boolean firstRoll) {
 		// if this is the first roll and you roll a 7 or 11
-		if (dice == 7 && firstRoll == true || dice == 11 && firstRoll == true) {
+		if (dice == 7 && firstRoll || dice == 11 && firstRoll) {
 			System.out.println("You rolled a " + dice + " on the first roll. You win");
 			return "win";
 			// if this is the first roll and you roll a 2, 3 or 12
-		} else if (dice == 2 && firstRoll == true || dice == 3 && firstRoll == true
-				|| dice == 12 && firstRoll == true) {
+		} else if (dice == 2 && firstRoll || dice == 3 && firstRoll || dice == 12 && firstRoll) {
 			System.out.println("You rolled a " + dice + " on the first roll. You lose");
 			return "lose";
 			// if its not the first roll and you roll a 7
-		} else if (firstRoll == false && dice == 7) {
+		} else if (!firstRoll && dice == 7) {
 			System.out.println("You rolled " + dice + ".\nYou lose!");
 			return "lose";
 			// if it gets pass the other logic and it's the first roll, play the game
@@ -51,7 +50,7 @@ public class RollDice {
 			playGame(dice(), firstRoll());
 			// if it's not the first roll and dice is not equal to points, print out and
 			// play game
-		} else if (firstRoll == false && dice != point) {
+		} else if (!firstRoll && dice != point) {
 			System.out.println("You rolled " + dice + " Your point is " + point);
 			playGame(dice(), firstRoll());
 			// if the dice and the points are equal, this should catch it and print out a
@@ -66,7 +65,7 @@ public class RollDice {
 
 	// this method is used to store the first roll;
 	public static int firstRollPoint(int i, boolean firstRoll) {
-		if (point == 0 && firstRoll == true) {
+		if (point == 0 && firstRoll) {
 			point = i;
 		}
 		return point;
--- src/dice/RollDiceTest.java
+++ src/dice/RollDiceTest.java
@@ -7,60 +7,60 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RollDiceTest {
-	RollDice roller = new RollDice();
-	// int rolled = roller.firstRollPoint(8, true);
 
 	@Test
 	@DisplayName("this should return true if it is the first roll")
 	void isThisTheFirstRollTest() {
-		assertTrue("this should return true if it is the first roll", roller.firstRoll() == true);
-		assertTrue("this is the second call, this should return false", roller.firstRoll() == false);
-		assertTrue("this is the third call, this should return false", roller.firstRoll() == false);
+		assertTrue("this should return true if it is the first roll", RollDice.firstRoll() == true);
+		assertTrue("this is the second call, this should return false", RollDice.firstRoll() == false);
+		assertTrue("this is the third call, this should return false", RollDice.firstRoll() == false);
 	}
 
 	@Test
 	@DisplayName("should generate a number between 2-12")
 	void diceLowestNumTest() {
-		assertTrue("should be greater than or equal to 2", roller.dice() > 1);
+		assertTrue("should be greater than or equal to 2", RollDice.dice() > 1);
 	}
 
 	@Test
 	@DisplayName("should generate a number between 2-12")
 	void diceLargestNumTest() {
-		assertTrue("should be greater than or equal to 2", roller.dice() > 1);
+		assertTrue("should be greater than or equal to 2", RollDice.dice() > 1);
 	}
 
 	@Test
 	@DisplayName("Should only store first roll")
 	void pointTest() {
-		assertEquals("this should return 5", 5, roller.firstRollPoint(5, true));
+		assertEquals("this should return 5", 5, RollDice.firstRollPoint(5, true));
 		assertEquals("this should return 5 and not 8 becuase it is not the first roll", 5,
-				roller.firstRollPoint(8, false));
-		roller.point = 0;
-		assertEquals("this should return 8", 8, roller.firstRollPoint(8, true));
+				RollDice.firstRollPoint(8, false));
+		RollDice.point = 0;
+		assertEquals("this should return 8", 8, RollDice.firstRollPoint(8, true));
 
 	}
 
 	@Test
 	@DisplayName("roller dice should return a win or lose and it's the first roll")
 	void playGameTest() {
-		assertEquals("this should return win if they roll a 7 on the first roll", roller.playGame(7, true), "win");
-		assertEquals("this should return win if they roll an 11 on the first roll", roller.playGame(11, true), "win");
-		assertEquals("this should return a lose if they roll a 2 on the first roll", roller.playGame(2, true), "lose");
-		assertEquals("this should return a lose if they roll a 3 on the first roll", roller.playGame(3, true), "lose");
-		assertEquals("this should return a lose if they roll a 12 on the first roll", roller.playGame(12, true),
+		assertEquals("this should return win if they roll a 7 on the first roll", RollDice.playGame(7, true), "win");
+		assertEquals("this should return win if they roll an 11 on the first roll", RollDice.playGame(11, true), "win");
+		assertEquals("this should return a lose if they roll a 2 on the first roll", RollDice.playGame(2, true),
+				"lose");
+		assertEquals("this should return a lose if they roll a 3 on the first roll", RollDice.playGame(3, true),
+				"lose");
+		assertEquals("this should return a lose if they roll a 12 on the first roll", RollDice.playGame(12, true),
 				"lose");
 	}
 
 	@Test
 	@DisplayName("test game play logic if it is NOT the first roll")
 	void playGameSecondRollTest() {
-		assertEquals("this should return lose if they roll a 7 and it is NOT the first roll", roller.playGame(7, false),
-				"lose");
-		roller.point = 0;
-		assertEquals("this sets first roll to 8", 8, roller.firstRollPoint(8, true));
+		assertEquals("this should return lose if they roll a 7 and it is NOT the first roll",
+				RollDice.playGame(7, false), "lose");
+		RollDice.point = 0;
+		assertEquals("this sets first roll to 8", 8, RollDice.firstRollPoint(8, true));
 		assertEquals("this should return a win if we roll an 8 and first roll is false", "win",
-				roller.playGame(8, false));
+				RollDice.playGame(8, false));
 	}
 
 }
